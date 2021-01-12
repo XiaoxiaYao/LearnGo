35f5aff47be346aa36aa46041e531578d591e201
michaelyaonz@gmail.com <yaoxiaoxia@easternbay.cn>
2021-01-12 23:32:46 +0800

    Use braces

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+// This is a package variable not a global variable
+var (
+	name      = "Mike"
+	firstName = "Michael"
+)
+
 func main() {
 	fmt.Println("Hello world")
+	fmt.Println(name)
+	fmt.Println(firstName)
 }
